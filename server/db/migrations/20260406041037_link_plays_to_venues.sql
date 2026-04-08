777fe33a554ffe131ab02560bd5c0515284d7fad
darrelhong <darrel.hong@gmail.com>
2026-04-08 09:57:29 +0800

    fix unique index

--- server/db/migrations/20260406041037_link_plays_to_venues.sql
+++ server/db/migrations/20260406041037_link_plays_to_venues.sql
@@ -2,7 +2,7 @@
 ALTER TABLE plays ADD COLUMN venue_id INTEGER REFERENCES venues(id);
 
 DROP INDEX IF EXISTS idx_plays_dedup;
-CREATE UNIQUE INDEX idx_plays_dedupe ON plays(host_name, starts_at, ends_at, sport, venue_id);
+CREATE UNIQUE INDEX idx_plays_dedupe ON plays(host_name, starts_at, ends_at, sport, level_min, level_max, venue_id);
 
 -- +goose Down
 DROP INDEX IF EXISTS idx_plays_dedupe;
